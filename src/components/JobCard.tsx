ac258287178fa2dffdf614c4598e1afaec0bd979
lingsat <lingsatpilingast@gmail.com>
2022-11-01 09:26:05 +0200

    adaptive for mobile devices and tablets

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -16,26 +16,27 @@ const JobCard: FC<JobCardProps> = ({ job, onToggleFavouriteJob }) => {
     return Math.round(diff);
   };
 
-
   const starsBlock = (
     <div className="card__stars flex justify-center">
-      <img src="./assets/images/Star.svg" alt="Star" />
-      <img src="./assets/images/Star.svg" alt="Star" />
-      <img src="./assets/images/Star.svg" alt="Star" />
-      <img src="./assets/images/Star.svg" alt="Star" />
-      <img src="./assets/images/Star.svg" alt="Star" />
+      <img className="w-3 sm:w-5" src="./assets/images/Star.svg" alt="Star" />
+      <img className="w-3 sm:w-5" src="./assets/images/Star.svg" alt="Star" />
+      <img className="w-3 sm:w-5" src="./assets/images/Star.svg" alt="Star" />
+      <img className="w-3 sm:w-5" src="./assets/images/Star.svg" alt="Star" />
+      <img className="w-3 sm:w-5" src="./assets/images/Star.svg" alt="Star" />
     </div>
   );
 
   return (
-    <div className="card flex justify-between my-2 px-4 py-6 rounded-lg">
-      <div className="card__img">
+    <div className="card flex justify-between my-2 px-4 py-4 sm:py-6 rounded-lg">
+      <div className="card__img mt-8 sm:mt-9 md:mt-0">
         <img src={job.pictures[0]} alt={job.name} />
       </div>
-      <div className="card__content flex gap-8 justify-between">
-        <div className="w-8/12">
+      <div className="card__content flex flex-col-reverse md:flex-row justify-between">
+        <div className="md:w-1/2 lg:w-7/12 xl:w-8/12">
           <Link to={`/job/${job.id}`}>
-            <h2 className="card__title mb-2 text-xl font-bold">{job.title}</h2>
+            <h2 className="card__title mb-2 text-lg leading-6 sm:text-xl sm:font-bold">
+              {job.title}
+            </h2>
           </Link>
           <p className="card__text mb-2 text-base">
             Department name • {job.name}
@@ -45,16 +46,18 @@ const JobCard: FC<JobCardProps> = ({ job, onToggleFavouriteJob }) => {
             {job.address}
           </p>
         </div>
-        <div className="w-4/12 flex gap-8 justify-end items-center">
-          {job.favourite ? starsBlock : null}
-          <div className="card__data h-full flex flex-col justify-between items-end">
+        <div className="md:w-1/2 lg:w-5/12 xl:w-4/12 mb-3 md:mb-0 flex gap-3 sm:gap-8 md:justify-end items-center">
+          {!job.favourite ? starsBlock : null}
+          <div className="card__data ml-auto md:ml-0 h-full flex flex-row-reverse md:flex-col justify-between items-center md:items-end">
             <img
-              className="card__icon w-4"
+              className="card__icon w-4 hidden sm:block ml-4"
               src="./assets/images/To_favourite.svg"
               alt="To Favourite"
               onClick={() => onToggleFavouriteJob(job.id)}
             />
-            <p className="card__text">Posted {getDateDiff()} days ago</p>
+            <p className="card__text text-sm sm:text-base">
+              Posted {getDateDiff()} days ago
+            </p>
           </div>
         </div>
       </div>
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -9,7 +9,7 @@ interface MainPageProps {
 
 const MainPage: FC<MainPageProps> = ({ jobs, onToggleFavouriteJob }) => {
   return (
-    <main className="max-container mx-auto px-5 py-6">
+    <main className="max-container mx-auto px-3 py-1 sm:px-5 sm:py-3">
       {jobs.map((job: IJob) => {
         return <JobCard key={job.id} job={job} onToggleFavouriteJob={onToggleFavouriteJob} />;
       })}
